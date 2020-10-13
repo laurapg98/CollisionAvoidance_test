55a5f50da48ba0daf9e94e239375dc0ef0ed553c
laurapg98 <laura.parga.lp@gmail.com>
2020-10-13 14:30:48 +0200

    arreglos

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from D435functions import start_D435, data_D435, exists_obstacle_ahead, stop_D435
 from DRONEfunctions import start_connection, stop_connection, stop_mission, save_mission, change_altitude, move_forward, add_current_waypoint, upload_mission
-from ALTIMETERfunctions import start_alt, stop_alt, getDistance, exists_obstacle_under
+from ALTIMETERfunctions import start_alt, stop_alt, getDistance_alt, exists_obstacle_under
 import time
 import serial
 import dronekit_sitl
@@ -10,7 +10,7 @@ flightaltitude = 2 #m ???????(invent)??????? m *********************************
 speed = 5 #m/s ???????(invent)??????? m *******************************************
 
 # Camera serial number (to identify it)
-serialnumber_D435 = 829212070982 # ******************* NÚM. DE SÈRIE DE LA CÀMERA *******************
+serialnumber_D435 = "829212070982" # ******************* NÚM. DE SÈRIE DE LA CÀMERA *******************
 
 # Camera number of pixels
 xmin = 0
@@ -46,7 +46,7 @@ try:
     while True:
 
         # Altitude data
-        altitude = getDistance(pipe_alt)
+        altitude = getDistance_alt(pipe_alt)
 
         # Check altitude
         while (altitude != flightaltitude):
