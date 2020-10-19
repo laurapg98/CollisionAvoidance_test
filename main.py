8c4ec3129068d2e5193f91ff2e953ab8eab4b39a
laurapg98 <laura.parga.lp@gmail.com>
2020-10-19 19:13:08 +0200

    ?

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import serial
 import dronekit_sitl
 
 # Import functions
-from D435functions import start_D435, data_D435, exists_obstacle_ahead, stop_D435
+from D435functions import start_D435, exists_obstacle_ahead, stop_D435
 from DRONEfunctions import start_connection, stop_connection, stop_mission, save_mission, change_altitude, move_forward, add_current_waypoint, upload_mission
 from ALTIMETERfunctions import start_alt, stop_alt, getDistance_alt, exists_obstacle_under
 
