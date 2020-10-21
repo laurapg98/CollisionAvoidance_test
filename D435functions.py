90deaea3a43329522f7f673374f9d6e831103b34
laurapg98 <laura.parga.lp@gmail.com>
2020-10-21 16:35:09 +0200

    ready to test without drone - with comments

--- D435functions.py
+++ D435functions.py
@@ -9,21 +9,27 @@ def start_D435(serialnumber):
     pipe_D435.start(cfg_D435) # start recording
     return pipe_D435
 
-# Establish (true, false) if there is an obstacle ahead
-def exists_obstacle_ahead(pipe_D435, securitydistance, xmin, ymin, xmax, ymax, minpixels):
+# Establish (True, False) if there is an obstacle ahead
+def exists_obstacle_ahead(pipe_D435, securitydistance, x_pixels, y_pixels, minpixels):
+
+    # Get depth data
     frames_D435 = pipe_D435.wait_for_frames() # frames
-    depthframes = frames_D435.get_depth_frames() # filter --> only depth frames
-    counter = 0 # number of pixels at a distance less than the security one
-    # frames matrix search
-    x = xmin
-    while x < xmax:
-        y = ymin
-        while y < ymax:
+    depthframes = frames_D435.get_depth_frame() # filter --> only depth frames
+
+    # Number of pixels at a distance less than the security one
+    counter = 0 
+
+    # Read depth data
+    x = 0
+    while x < x_pixels:
+        y = 0
+        while y < y_pixels:
             if(depthframes.get_distance(x,y) != 0 and depthframes.get_distance(x,y) < securitydistance): 
                 counter+=1
             y+=1
         x+=1
-    # establish if there is or not an obstacle
+    
+    # Establish if there is (True) (False) or not an obstacle
     if counter > minpixels:
         return True
     else:
--- main.py
+++ main.py
@@ -1,146 +1,100 @@
 import time
-import serial
+#import serial
 from dronekit import connect, VehicleMode, Command, LocationGlobal
-import dronekit_sitl
-from commonFunctions import *
 
 # Import functions
 from D435functions import start_D435, exists_obstacle_ahead, stop_D435
-from DRONEfunctions import start_connection, stop_connection, stop_mission, save_mission, change_altitude, move_forward, add_current_waypoint, upload_mission
-from ALTIMETERfunctions import start_alt, stop_alt, getDistance_alt, exists_obstacle_under
+from DRONEfunctions import start_connection, stop_connection, stop_mission, save_mission, get_flight_altitude, change_altitude, move_forward, add_current_waypoint, test_mission, exists_obstacle_under, upload_mission
 
 # Flight parameters
-# m ???????(invent)??????? m *******************************************
-flightaltitude = 2
-# m/s ???????(invent)??????? m *******************************************
-speed = 5
+flightaltitude = 1 # m *******************************************
+speed = 5 # m/s *******************************************
 
-# Camera serial number (to identify it)
+# Camera parameters
 serialnumber_D435 = "829212070982"
-
-# Camera number of pixels
-xmin = 0
-xmax = 640
-ymin = 0
-ymax = 480
-
-# Connection with altimeter
-pipe_alt = None
-while (pipe_alt == None):
-    pipe_alt = start_alt()
+x_pixels = 640
+y_pixels = 480
 
 # Connection with D435 camera
 pipe_D435 = None
 while pipe_D435 == None:
     pipe_D435 = start_D435(serialnumber_D435)
 
-
 # Connection with drone
 vehicle = None
 while vehicle == None:
     vehicle = start_connection()
-while not vehicle.is_armable:
-    time.sleep(1)
 
-# Upload mission from file
-# upload_mission(vehicle, filename)
-global cmds
-
-cmds = vehicle.commands
-
-lat = vehicle.location.global_frame.lat
-lon = vehicle.location.global_frame.lon
-heading = vehicle.heading
-
-finalPoint = pointRadialDistance(lat, lon, heading, 0.1)
-
-# TAKEOFF
-cmds.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
-                 mavutil.mavlink.MAV_CMD_DO_SET_HOME, 0, 0, 1, 0, 0, 0, 0, 0, flightaltitude))
-cmds.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
-                 mavutil.mavlink.MAV_CMD_DO_SET_HOME, 0, 0, 1, 0, 0, 0, 0, 0, flightaltitude))
-cmds.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
-                 mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 20, 0, 0, 0, 0, 0, flightaltitude))
-cmds.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
-                 mavutil.mavlink.MAV_CMD_DO_CHANGE_SPEED, 0, 0, 0, speed, 0, 0, 0, 0, flightaltitude))
-# MISSION
-
-cmds.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
-                 mavutil.mavlink.MAV_CMD_NAV_WAYPOINT, 0, 0, 0, 0, 0, 0, finalPoint.lat, finalPoint.lon, flightaltitude))
-
-# LANDING
-
-cmds.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
-                 mavutil.mavlink.MAV_CMD_DO_LAND_START, 0, 0, 0, 0, 0, 0, finalPoint.lat, finalPoint.lon, flightaltitude))
-
-cmds.add(Command(0, 0, 0, mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
-                 mavutil.mavlink.MAV_CMD_NAV_LAND, 0, 0, 0, 0, 0, 0, finalPoint.lat, finalPoint.lon, flightaltitude))
-
-cmds.upload()
-
-vehicle.mode = VehicleMode("AUTO")
-vehicle.armed = True
-
+# Upload mission
+test_mission(vehicle, flightaltitude, speed, 100)
 
 try:
     while True:
 
         # Altitude data
-        # altitude = getDistance_alt(pipe_alt)
-        altitude = vehicle.sonarrange
+        altitude = vehicle.rangefinder.distance
 
-        # Check altitude
-        while (altitude != flightaltitude):
-            change_altitude(vehicle, flightaltitude - altitude)
+        # Check altitude (maximum 5% error)
+        while (altitude >= 0.95 * flightaltitude and altitude <= 1.05 * flightaltitude):
+            #get_flight_altitude(vehicle, flightaltitude)
+            print("Current altitude: " + str(altitude) + " m | Flight altitude: " + str(flightaltitude) + " m")
+            altitude = vehicle.rangefinder.distance
 
         # Security parameters
-        # ???????(invent)??????? m *******************************************
-        securitydistance = 2
-        # ???????(invent)??????? pixels  *******************************************
-        minpixels = 100
+        securitydistance = 2 # m *******************************************
+        minpixels = 1000 # pixels *******************************************
 
         # Obstacle detected
-        if (exists_obstacle_ahead(pipe_D435, securitydistance, xmin, ymin, xmax, ymax, minpixels) == True):
-            # saves the initial mssion
+        if (exists_obstacle_ahead(pipe_D435, securitydistance, x_pixels, y_pixels, minpixels) == True):
+            print("Obstacle detected")
+            print("Starting avoidance mission")
+
+            # Save initial mssion
             initialmission = save_mission(vehicle)
-            # stops the initial mission
-            stop_mission(vehicle)
-            # obstacle avoidance loop
-            # ???????(invent)??????? m  *******************************************
-            Ah = 0.5
-            # ???????(invent)??????? m  *******************************************
-            Ad = 0.5
-            # ???????(invent)??????? m  *******************************************
-            d = 7
+            print("Initial mission saved")
+
+            # Stops the initial mission
+            #stop_mission(vehicle)
+            print("Initial mission stopped")
+
+            # Obstacle avoidance parameters
+            Ah = 0.5 # m ******************************************* IT MUST BE SMALLER THAN SECURITY DISTANCE
+            Ad = 0.5 # m ******************************************* IT MUST BE SMALLER THAN SECURITY DISTANCE
+            d = 3.5 # m ******************************************* IT MUST BE HIGHER THAN SECURITY DISTANCE
+
+            # Obstacle avoidance loop
             counter = 0
-            while (exists_obstacle_ahead(pipe_D435, securitydistance, xmin, ymin, xmax, ymax, minpixels) == True):
-                while (exists_obstacle_ahead(pipe_D435, securitydistance, xmin, ymin, xmax, ymax, minpixels) == True):
-                    while (exists_obstacle_ahead(pipe_D435, d, xmin, ymin, xmax, ymax, minpixels) == True):
-                        change_altitude(vehicle, Ah)
+            while (exists_obstacle_ahead(pipe_D435, securitydistance, x_pixels, y_pixels, minpixels) == True):
+                print("[while 1] Obstacle at less than " + str(securitydistance) + " m")
+                while (exists_obstacle_ahead(pipe_D435, securitydistance, x_pixels, y_pixels, minpixels) == True):
+                    print("[while 2] Obstacle at less than " + str(securitydistance) + " m")
+                    while (exists_obstacle_ahead(pipe_D435, d, x_pixels, y_pixels, minpixels) == True):
+                        print("Obstacle at less than " + str(d) + " m")
+                        #change_altitude(vehicle, Ah)
+                        print("Increase altitude " + str(Ah) + " m")
                         counter += 1
-                    move_forward(vehicle, d - securitydistance, speed)
+                    #move_forward(vehicle, d - securitydistance, speed)
+                    print("Move " + str(d - securitydistance) + " m")
                 obstacle_under = True
-                while (exists_obstacle_ahead(pipe_D435, securitydistance + Ad, xmin, ymin, xmax, ymax, minpixels) == False and obstacle_under == True):
-                    if (exists_obstacle_under(pipe_alt, flightaltitude, counter*Ah) == False):
-                        change_altitude(vehicle, -counter*Ah)
+                while (exists_obstacle_ahead(pipe_D435, securitydistance + Ad, x_pixels, y_pixels, minpixels) == False and obstacle_under == True):
+                    if (exists_obstacle_under(vehicle, flightaltitude, counter*Ah) == False):
+                        #get_flight_altitude(vehicle, flightaltitude)
+                        print("Decrease altitude to fligh altitude")
                         obstacle_under = False
                     else:
-                        move_forward(vehicle, Ad, speed)
-            # add current point to initial mission
-            initialmission_edit = add_current_waypoint(vehicle, initialmission)
+                        print("Obstacle under the drone")
+                        #move_forward(vehicle, Ad, speed)
+                        print("Move " + str(Ad) + " m")
+
+            # Return to initial mission
+            print("Avoidance mission done. Going on with initial mission.")
+            initialmission_edit = add_current_waypoint(vehicle, initialmission, flightaltitude)
             upload_mission(vehicle, initialmission_edit)
 
 finally:
-
-    # Connection with altimeter
-    stop_alt(pipe_alt)
 
     # Connection with camera
     stop_D435(pipe_D435)
 
     # Connection with drone
     stop_connection(vehicle)
-
-    # Stops simulation
-    if sitl is not None:
-        sitl.stop()
